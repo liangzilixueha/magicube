7c80ffb1d7fb149c62e0b584b5aff09d3cdf5c06
liangzilixueha <542876162@qq.com>
2022-10-10 21:09:28 +0800

    预览画面

--- app/src/main/java/com/liangzilixueha/magicube/MainActivity.java
+++ app/src/main/java/com/liangzilixueha/magicube/MainActivity.java
@@ -1,31 +1,57 @@
 package com.liangzilixueha.magicube;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
 
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import com.liangzilixueha.magicube.databinding.ActivityMainBinding;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Camera mCamera;//相机
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setUpCamera();
-    }
+        SurfaceView preview = binding.preview;
+        //设置preview为相机预览画面
+        preview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                //打开相机
+                mCamera = Camera.open();
+                try {
+                    //设置预览画面,旋转90度
+                    mCamera.setPreviewDisplay(holder);
+                    mCamera.setDisplayOrientation(90);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
-    private void setUpCamera() {
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                //开始预览
+                mCamera.startPreview();
+            }
 
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                //停止预览
+                mCamera.stopPreview();
+                //释放相机
+                mCamera.release();
+            }
+        });
     }
 }
